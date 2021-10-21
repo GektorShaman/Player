bd1c9b91aa12dad4cad78eb250720faa961d99af
GektorShaman <zhadan.viktor@gmail.com>
2021-10-21 14:03:55 +0300

    Add validation for edittext

--- app/src/main/java/com/player/RegistrationAdapter.kt
+++ app/src/main/java/com/player/RegistrationAdapter.kt
@@ -1,7 +1,10 @@
 package com.player
 
 import android.graphics.Color
+import android.text.Editable
 import android.text.InputType
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,9 +13,11 @@ import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class RegistrationAdapter (private val regDataset: List<String>,private val footerClick: (View, Int, Int)->Unit, val itemCheck:()->Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    private val validateList: ArrayList<EditText?> = ArrayList<EditText?>()
     private val regDataQuantity: Int = 4
 
     override fun getItemViewType(position: Int): Int {
@@ -34,7 +39,10 @@ class RegistrationAdapter (private val regDataset: List<String>,private val foot
 
     private fun <T : RecyclerView.ViewHolder> T.onClick(event: (view: View, position: Int, type: Int) -> Unit): T {
         itemView.setOnClickListener {
-            event.invoke(it, getAdapterPosition(), itemViewType)
+            if (validateList.size > 2)  {
+                if (isValidRegistration()) event.invoke(it, getAdapterPosition(), itemViewType)
+            }
+            else if (isValidLogin()) event.invoke(it, getAdapterPosition(), itemViewType)
         }
         return this
     }
@@ -90,12 +98,15 @@ class RegistrationAdapter (private val regDataset: List<String>,private val foot
                     4 -> {
                         holder.editTextView?.inputType =
                             InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                        holder.editTextView?.isEnabled = false
                     }
                 }
             }
             else if (position==2) holder.editTextView?.inputType =
                 InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
 
+            holder.editTextView?.addTextChangedListener(FieldValidation(holder.editTextView!!))
+            validateList.add(holder.editTextView)
         } else if (holder is FooterViewHolder) {
             holder.onClick(footerClick)
             holder.footerTextView?.text=regDataset[position]
@@ -145,5 +156,111 @@ class RegistrationAdapter (private val regDataset: List<String>,private val foot
         private const val TYPE_FOOTER = 2
     }
 
+    inner class FieldValidation(private val view: EditText) : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            when (view.inputType) {
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS -> validateEmail()
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD -> validatePassword()
+                else -> validateLogin()
+            }
+        }
+    }
 
+    fun isValidRegistration():Boolean
+    {
+        return (validateEmail() and validatePassword() and validateConfirmPassword() and validateLogin())
+    }
+
+    fun isValidLogin(): Boolean
+    {
+        return (validatePassword() and validateLogin())
+    }
+
+    fun validateLogin (): Boolean {
+
+        if (validateList[0]?.text.toString().trim().isEmpty())
+        {
+            validateList[0]?.error= "Requered Field"
+            validateList[0]?.requestFocus()
+            return false
+        }
+        return true
+    }
+
+    fun validateEmail(): Boolean {
+        if (validateList.size>2) {
+            if (validateList[1]?.text.toString().trim().isEmpty()) {
+                validateList[1]?.error = "Requered Field"
+                validateList[1]?.requestFocus()
+                return false
+            } else if (!validateList[1]?.text.toString().isEmailValid()) {
+                validateList[1]?.error = "Invalid Email"
+                validateList[1]?.requestFocus()
+                return false
+            }
+        }
+        return true
+
+    }
+
+    private fun String.isEmailValid(): Boolean {
+        return !TextUtils.isEmpty(this) && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
+
+    fun validatePassword(): Boolean
+    {
+        val elementPosition: Int
+        if (validateList.size>2)  elementPosition=2
+        else elementPosition = 1
+
+        if (validateList[elementPosition]?.text.toString().trim().isEmpty())
+        {
+            validateList[elementPosition]?.error= "Requered Field"
+            validateList[elementPosition]?.requestFocus()
+            return false
+        } else if (validateList[elementPosition]?.text.toString().length < 6)
+        {
+            validateList[elementPosition]?.error= "Password must be more than 6 symbols"
+            validateList[elementPosition]?.requestFocus()
+            return false
+        } else if (!validateList[elementPosition]?.text.toString().contains("[0-9]".toRegex()))
+        {
+            validateList[elementPosition]?.error= "Password must have at least 1 digit"
+            validateList[elementPosition]?.requestFocus()
+            return false
+        } else if (!validateList[elementPosition]?.text.toString().contains("[A-Z]".toRegex()))
+        {
+            validateList[elementPosition]?.error= "Password must have upper case"
+            validateList[elementPosition]?.requestFocus()
+            return false
+        } else if (!validateList[elementPosition]?.text.toString().contains("[a-z]".toRegex())) {
+            validateList[elementPosition]?.error = "Password must have lower case"
+            validateList[elementPosition]?.requestFocus()
+            return false
+        } else if (!validateList[elementPosition]?.text.toString().contains("[!-/]".toRegex()))
+        {
+            validateList[elementPosition]?.error= "Password must have special symbols"
+            validateList[elementPosition]?.requestFocus()
+            return false
+        }
+        if (elementPosition==2) validateList[elementPosition+1]?.isEnabled = true
+        return true
+    }
+
+    private fun validateConfirmPassword(): Boolean
+    {
+        if (validateList[3]?.text.toString().trim().isEmpty())
+        {
+            validateList[3]?.error= "Requered Field"
+            validateList[3]?.requestFocus()
+            return false
+        } else if (validateList[2]?.text.toString() != validateList[3]?.text.toString())
+        {
+            validateList[3]?.error= "Passwords don't match"
+            return false
+        }
+        return true
+    }
 }
